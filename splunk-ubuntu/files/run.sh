b219f1fe5df0a92784e6517eedc4df60361316b3
Andreas Gies <andreas@wayofquality.de>
2021-01-08 18:31:42 +0100

    Updating splunk image to be executable in Openshift

--- splunk-ubuntu/files/run.sh
+++ splunk-ubuntu/files/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -l
 
+groups 
+
 SPLUNK_HOME=/opt/splunk
 
-mkdir -p ${SPLUNK_HOME}/var/log/splunk 
-nohup ${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt 2>&1 > /dev/null &
-touch ${SPLUNK_HOME}/var/log/splunk/splunkd.log
-tail -f ${SPLUNK_HOME}/var/log/splunk/splunkd.log
+umask 002
+
+ mkdir -p ${SPLUNK_HOME}/var/log/splunk 
+ls -al $SPLUNK_HOME
+# nohup ${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt 2>&1 > /dev/null &
+# touch ${SPLUNK_HOME}/var/log/splunk/splunkd.log
+# tail -f ${SPLUNK_HOME}/var/log/splunk/splunkd.log
 
