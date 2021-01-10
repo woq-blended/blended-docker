5359ff1cfcd91ba42b5a428adae1980204eb781d
Andreas Gies <andreas@wayofquality.de>
2021-01-10 11:57:59 +0100

    Reenabling splunk start

--- splunk-ubuntu/files/run.sh
+++ splunk-ubuntu/files/run.sh
@@ -8,7 +8,7 @@ umask 002
 
  mkdir -p ${SPLUNK_HOME}/var/log/splunk 
 ls -al $SPLUNK_HOME
-# nohup ${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt 2>&1 > /dev/null &
-# touch ${SPLUNK_HOME}/var/log/splunk/splunkd.log
-# tail -f ${SPLUNK_HOME}/var/log/splunk/splunkd.log
+nohup ${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt 2>&1 > /dev/null &
+touch ${SPLUNK_HOME}/var/log/splunk/splunkd.log
+tail -f ${SPLUNK_HOME}/var/log/splunk/splunkd.log
 
