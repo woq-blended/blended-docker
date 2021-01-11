98574e9ebe55b33cd6fa313b0a5016b245fbcf13
Andreas Gies <andreas@wayofquality.de>
2021-01-11 13:18:20 +0100

    download script to downlaod and configure splunk files for docker image

--- splunk-ubuntu/download.sh
+++ splunk-ubuntu/download.sh
@@ -1,2 +1,13 @@
 #!/bin/bash 
-wget -O splunk-8.1.1-08187535c166-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.1&product=splunk&filename=splunk-8.1.1-08187535c166-Linux-x86_64.tgz&wget=true'
+cd files
+
+SPLUNK_TGZ=splunk-8.1.1-08187535c166-Linux-x86_64.tgz
+
+umask 002
+wget -O $SPLUNK_TGZ 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.1&product=splunk&filename='$SPLUNK_TGZ'&wget=true'
+tar -xzf $SPLUNK_TGZ
+rm -f $SPLUNK_TGZ
+chgrp -R root splunk 
+chmod -R g+srwX splunk
+cd ..
+
