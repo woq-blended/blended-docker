b05724ed198802e47ea78878f31b3ed5783281ab
Andreas Gies <andreas@wayofquality.de>
2021-01-11 12:36:26 +0100

    Adding download script to splunk image definition

---
+++ splunk-ubuntu/download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+wget -O splunk-8.1.1-08187535c166-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.1&product=splunk&filename=splunk-8.1.1-08187535c166-Linux-x86_64.tgz&wget=true'
