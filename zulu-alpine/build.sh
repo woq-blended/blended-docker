a3b18e279a9a4f3f0818bdde93733d000ebc6ca9
Andreas Gies <andreas@wayofquality.de>
2021-01-12 13:52:37 +0100

    Refactoring zulu base alpine image

---
+++ zulu-alpine/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+cd files
+
+JRE_NAME="zulu8.50.0.51-ca-jre8.0.275-linux_musl_x64"
+
+umask 002
+wget -O "$JRE_NAME.tar.gz" 'https://cdn.azul.com/zulu/bin/'$JRE_NAME'.tar.gz'
+tar -xzf "$JRE_NAME.tar.gz"
+rm -f $JRE_NAME.tar.gz
+chgrp -R root $JRE_NAME
+chmod -R g+srwX $JRE_NAME
+cd ..
+
+docker build --build-arg JRE_NAME=$JRE_NAME -t blended/zulu-alpine/$(cat version.txt) .
