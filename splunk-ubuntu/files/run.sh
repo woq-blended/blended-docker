e282757668303705cc95cb2ef4b40fc44a5ed8cd
Andreas Gies <andreas@wayofquality.de>
2021-01-19 17:23:34 +0100

    Update splunk image to Open Shift compatibility

--- splunk-ubuntu/files/run.sh
+++ splunk-ubuntu/files/run.sh
@@ -4,6 +4,8 @@ SPLUNK_HOME=/opt/splunk
 
 umask 002
 
+tar -C /tmp -cf - splunk  | tar -C /opt/splunk --strip-components=1 -xvf -
+
 mkdir -p ${SPLUNK_HOME}/var/log/splunk 
 ls -al $SPLUNK_HOME
 nohup ${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt 2>&1 > /dev/null &
